944be9cd226bbdc4fe279460cc8d105c59a3c9cd
Rafael <marquestec@gmail.com>
2022-05-12 09:43:08 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 
 
@@ -9,9 +12,11 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1,"books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@email.com",new Date(),3000.00 , obj);
+		
+		System.out.println(seller);
 	}
 }
 
-
-
+                  
